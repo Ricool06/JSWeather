046b7a53044230349d7d4a8e1677f71a06cd6f1b
Ryan Perry <ricoolohsix@hotmail.com>
2016-01-23 14:29:48 +0000

    On branch dev Changes to be committed:
    modified:   css/default.css
    modified:   index.html
    modified:   js/drawloop.js
    modified:   js/rain.js
    new file:   js/snow.js
    modified:   js/weatherControls.js

--- js/weatherControls.js
+++ js/weatherControls.js
@@ -8,7 +8,7 @@ var shadeIncrement = Math.round(255 / buttons.length);
 var clickedButton = buttons[0];
 
 for(var i = 0; i < buttons.length; i++){
-    buttons[i].style.backgroundColor = "rgba(" + br + ", 0, " + bb + ", 0.4)";
+    buttons[i].style.backgroundColor = "rgba(" + br + ", 120, " + bb + ", 0.4)";
     buttons[i].plainColor = buttons[i].style.backgroundColor;
     buttons[i].addEventListener("mouseleave", leaveColor);
     buttons[i].addEventListener("mouseenter", enterColor);
@@ -16,11 +16,9 @@ for(var i = 0; i < buttons.length; i++){
 
     br = br + shadeIncrement;
     bb = bb - shadeIncrement;
-
-    console.log(buttons[i]);
 }
 
-clickedButton.style.backgroundColor = "rgba(120,120,120,0.5)";
+clickedButton.style.backgroundColor = "rgba(120,220,120,0.5)";
 
 function changeWeatherType(e) {
     var button = e.currentTarget;
@@ -29,12 +27,12 @@ function changeWeatherType(e) {
     clickedButton.style.backgroundColor = clickedButton.plainColor;
 
     clickedButton = e.currentTarget;
-    clickedButton.style.backgroundColor = "rgba(120,120,120,0.5)";
+    clickedButton.style.backgroundColor = "rgba(120,220,120,0.5)";
 }
 
 function enterColor(e){
     if(e.currentTarget != clickedButton){
-        e.currentTarget.style.backgroundColor = "rgba(100,100,100,0.5)";
+        e.currentTarget.style.backgroundColor = "rgba(100,220,100,0.5)";
     }
 }
 
